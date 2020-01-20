5040ff136055961658e487ad98543c2ea7afaf66
Jiwon <chloe326o.o@gmail.com>
2020-01-20 23:58:17 +0900

    #2 3 map Recap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React from 'react';
 
-function Food({fav}){
-  console.log(fav)
-  return <h1>I like {fav}!!!!</h1>
-}
 
+const foodIlike = [
+    {
+      id :1,
+      name : "Kimchi",
+      image :
+       "https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg"
+  },{
+      id:2,
+      name : "samgyubsal",
+      image : 
+       "https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg"
+  }
+]
+
+function Food({name, picture}){
+  console.log(name)
+  return <div>
+    <h2>I  like {name}!!!!</h2>
+    <img src = {picture} />
+    </div>
+}
 
 function App() {
   return (
     <div>
     <h1>Hi</h1>
-    <Food fav="kimchi"/>
-    <Food fav="ramen"/>
-    <Food fav="pasta"/>
-    <Food fav="pizza"/>
-  </div>
+    {foodIlike.map( dish => (
+      <Food key={dish.id} name={dish.name} picture={dish.image} />
+    )
+    )}
+     </div>
   );
 
 }
